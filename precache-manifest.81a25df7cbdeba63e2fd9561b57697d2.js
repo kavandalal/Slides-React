2480da44e0a0c23babe3abf891e316483c48e4a1
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-08-10 20:37:45 +0530

    Updates

---
+++ precache-manifest.81a25df7cbdeba63e2fd9561b57697d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e40f37ff9cea73b0775387995c71d5",
+    "url": "/Slides-React/index.html"
+  },
+  {
+    "revision": "284364716a7176e919ec",
+    "url": "/Slides-React/static/css/main.bc1ddbdd.chunk.css"
+  },
+  {
+    "revision": "d4074e06e16d2154372e",
+    "url": "/Slides-React/static/js/2.8910e368.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Slides-React/static/js/2.8910e368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284364716a7176e919ec",
+    "url": "/Slides-React/static/js/main.1b4b5fdf.chunk.js"
+  },
+  {
+    "revision": "9072dbdd5d99cb316415",
+    "url": "/Slides-React/static/js/runtime-main.44de02e4.js"
+  }
+]);
