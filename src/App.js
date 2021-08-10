729af281efa4376b4bf2e2d963d62e0c0e8c5bf2
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-08-10 20:26:56 +0530

    1st deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, people]);
+	useEffect(() => {
+		let slider = setInterval(() => {
+			setIndex(index + 1);
+		}, 4000);
+		//clean up function needed as if you will click next/back arrow mutiple time both the useEffect will work and create aa mess
+		return () => {
+			clearInterval(slider);
+		};
+	}, [index]);
+	return (
+		<section className='section'>
+			<div className='title'>
+				<h2>
+					<span>/</span>reviews
+				</h2>
+			</div>
+			<div className='section-center'>
+				{people.map((person, personIndex) => {
+					const { id, image, name, title, quote } = person;
+					let position = 'nextSlide';
+					if (personIndex === index) {
+						position = 'activeSlide';
+					}
+					if (
+						personIndex === index - 1 ||
+						(index === 0 && personIndex === people.length - 1)
+					) {
+						position = 'lastSlide';
+					}
+					return (
+						<article className={position} key={id}>
+							<img src={image} alt={name} className='person-img' />
+							<h4>{name}</h4>
+							<p className='title'>{title}</p>
+							<p className='text'>{quote}</p>
+							<FaQuoteRight className='icon' />
+						</article>
+					);
+				})}
+				<button
+					className='prev'
+					onClick={() => {
+						setIndex(index - 1);
+					}}>
+					<FiChevronLeft />
+				</button>
+				<button
+					className='next'
+					onClick={() => {
+						setIndex(index + 1);
+					}}>
+					<FiChevronRight />
+				</button>
+			</div>
+		</section>
+	);
 }
 
 export default App;
